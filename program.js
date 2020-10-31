84c6197bc1d9cb5769a534426925df85740663ab
Lorena Rojas <lorenagabby4@gmail.com>
2020-10-31 07:32:57 -0500

    basic-Web is finished

--- program.js
+++ program.js
@@ -1,10 +1,20 @@
-function changeMessage(elementoArr) {
-    
-    setInterval(function(indexOfTrue=locationOfMessage(elementoArr)){ 
-        changeClass(indexOfTrue,"oculto");
-    }, 3000);
+function changeMessage(elementoArr) {  
+    setInterval(function(indexOfTrue=locationOfMessage(elementoArr)){         
+        document.getElementById(`op${indexOfTrue+1}`).classList.add("d0");
+        setTimeout(() => {
+            document.getElementById(`op${indexOfTrue+1}`).addEventListener("transitionend",changeClass(indexOfTrue,"oculto"));
+            setTimeout(() => {
+                if(indexOfTrue==elementoArr.length-1){
+                    document.getElementById(`op${1}`).classList.remove("d0");
+            
+                }else{
+                    document.getElementById(`op${indexOfTrue+2}`).classList.remove("d0");
+                }  
+            }, 1000);
+            
+        }, 1000);
+    }, 5000);
 }
-
 function locationOfMessage(arr) {
     let cantidad = arr.length;
     let array=[];
@@ -12,22 +22,19 @@ function locationOfMessage(arr) {
         arr.forEach((element,index)=> {
             array[index]=document.getElementById(`op${index+1}`).classList.contains("oculto");
     });
-    console.log(array); 
     return array.indexOf(false);
     }
 }
 function changeClass(indexOfTrue,clase){
     document.getElementById(`op${indexOfTrue+1}`).classList.add(clase);
-    console.log(document.getElementById(`op${indexOfTrue+1}`));
     if(indexOfTrue==elementoArr.length-1){
-        console.log("1");
         document.getElementById(`op${1}`).classList.remove(clase);
     }else{
-        console.log("2");
         document.getElementById(`op${indexOfTrue+2}`).classList.remove(clase);
-        console.log(document.getElementById(`op${indexOfTrue+2}`));
     }
+    console.log(`op${indexOfTrue+1}`);
 }
+
 let elemento=document.querySelectorAll(".cincuenta");
 let elementoArr = Array.from(elemento);
 changeMessage(elementoArr);
